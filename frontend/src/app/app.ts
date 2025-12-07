d7c6821a304c0e03dbb65f5959319111b07faa9a
lucas <onlyuzard@gmail.com>
2025-12-08 02:19:10 +0800

    fix: fixed breadCrumb disappear after applicationo build

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,10 @@
-import { Component, inject, signal, WritableSignal } from '@angular/core';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet, Event as RouterEvent } from '@angular/router';
 import { WindowServicee } from './services/window-servicee';
 import { CustomeTitleBar } from './layout/custome-title-bar/custome-title-bar';
 import { BreadCrumbs } from './layout/bread-crumbs/bread-crumbs';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { filter, map } from 'rxjs';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -19,11 +19,29 @@ export class App {
     protected readonly title = signal('frontend');
     protected isWidgetMode!: WritableSignal<boolean>;
     private router = inject(Router);
+    // private routerSubscription: Subscription | undefined;
+    // currentUrl = signal('');
 
+    // ngOnInit(): void {
+    //     this.routerSubscription = this.router.events
+    //         .pipe(
+    //             filter(
+    //                 (event: RouterEvent): event is NavigationEnd => event instanceof NavigationEnd
+    //             ),
+    //             map((e: NavigationEnd) => e.urlAfterRedirects)
+    //         )
+    //         .subscribe((path: string) => {
+    //             this.currentUrl.set(path);
+    //         });
+    // }
+
+    // ngOnDestroy(): void {
+    //     this.routerSubscription?.unsubscribe();
+    // }
     currentUrl = toSignal(
         this.router.events.pipe(
             filter((e): e is NavigationEnd => e instanceof NavigationEnd),
-            map((e: NavigationEnd) => e.url)
+            map((e: NavigationEnd) => e.urlAfterRedirects)
         ),
         { initialValue: this.router.url }
     );
